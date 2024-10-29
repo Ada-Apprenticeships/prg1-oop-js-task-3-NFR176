e06bf43123a15dc7358857df20a1559c16ce8f95
NFR176 <nevaeh.foster-ricket@ada.ac.uk>
2024-10-29 17:25:42 +0000

    v2 all passes /comments-need to refactor

--- oop.js
+++ oop.js
@@ -1,109 +1,94 @@
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
-function validInteger (value) { // value can be a string or a number (integer)
-  const num = Number(value);
-  return Number.isInteger(num) && num > 0;
-}  
+function validInteger (value) { // accepts a string or numeric value and return a boolean- showing if the 'value' can be positive
+  return Number.isInteger(parseInt(value))  && String(parseInt (value)) === String(value) && Number(value) >=0;
+}  // parseint converts a string to an integer-converts (value) to an integer
 
 
-function validatePriority(priority) { // value can be a string or a number (integer)
+function validatePriority(priority) { // accept a string or a numerical value- then return[1,3,5,7]
   const validPriority= Number(priority);
   return[1, 3, 5, 7].includes(validPriority) ? validPriority : PRIORITY["LOW"]
-}
-console.log(validatePriority("3"));
+}//if value is invalid- print 'LOW'
 
 function todaysDate () {
-  return new Date().toLocaleString('en-GB', {
-    timeZone: 'UTC',
-    hour12: false,
-    year: 'numeric',
-    month: '2-digit',
-    day:  '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-}).replace(',','');
+  return new Date().toLocaleString('en-GB', { //tolocalestring converts the date object to a string - en-gb just refers to the dd/mm/yy format
+    timeZone: 'UTC', hour12: false, //appears as false because it should be in a 24 hour clock format
+    year: 'numeric', month: '2-digit',//2-digit- thats how it will be displayed 09/07 etc etc
+    day:  '2-digit', hour: '2-digit',
+    minute: '2-digit', second: '2-digit',
+}).replace(',','');//removes comma that would seperate date and time
 
 }
-
 
 class Task  {
   _added;
   _title;
   _priority;
   
-  constructor(added, title, priority=0) {
-    this._added= new Date();
+  constructor(title, priority) {
+    this._added= todaysDate();
     this._title=title;
-    this._priority=priority;
+    this._priority=validatePriority(priority);
   
   }
-  
+
   get added() {
-      return this._added;
-    
-  }
-  get title() {
-      return this._title
-  } 
-  set title(title) {
-    this._title = title;
-  }
-  
-  get priority() {
-      return this._priority
-    
-  }
-  set priority(priority) {
-    if (priority >=0 && priority <= 7) {
-      this._priority = priority;
-    } else {
-      throw Error('Invalid priority as it must be in the range of 0-7.')
-    }
-  
-  }
-  
+  return this._added;
+
 }
- 
-class ToDo {
+get title() {
+  return this._title
+} 
+
+get priority() {
+  return this._priority;  
+}
+
+set priority(priority) {
+ this._priority = validatePriority(priority);
+
+}
+}
+
+ class ToDo {
   constructor() {
-    this.tasks = [];
+    this.tasks = []//empty array to hold all the tasks
   }
-  add(passedTask) {
-    this.tasks.push(passedTask);
+  add(task) {
+    if (task instanceof Task) {
+    this.tasks.push(task);
     return this.tasks.length;
-    }
-
-  list(priority = 0) {
-    const filteredTasks = priority === 0
-    ? this.tasks //true
-    : this.tasks.filter(task => task.priority === priority);
-
-    return filteredTasks.map(task => [task.added, task.title, task.priority]);
-    }
-  
-
-   delete(taskTitle) {
-    const taskIndex = this.tasks.findIndex(task => task.title === taskTitle);
-    if (taskIndex !== -1) {
-    this.tasks.splice(taskIndex, 1);
-    }
-    return this.tasks.length;
-    }
-  
-  task(taskTitle) {
-    const taskPresent = this.tasks.find(task=> task.title === taskTitle);
-    if(taskPresent) {
-      return taskPresent;
     } else {
-      throw new Error(`Task '${taskTitle} not present`);
-     }
-   }
+      throw new Error (`Invalid Task instance`);
+    }
   }
+  remove(title){
+    const taskIndex = this.tasks.findIndex(task => task.title.toLowerCase() === title.toLowerCase());
+    if (taskIndex !== -1) {
+      this.tasks.splice(taskIndex, 1);
+      return true;
+    }
+    return false;
 
-
-// Leave this code here for the automated tests
+  }
+  list(priority = 0) {
+    return this.tasks
+    .filter(task => priority === 0 || task.priority === priority)
+    .map(task =>[task.added, task.title,task.priority]);
+ }
+ task(title) {
+    const task = this.tasks.find(task => task.title.toLowerCase() === title.toLowerCase());
+    if (!task) {
+      throw new Error(`Task '${title}' Not Found`);
+  }
+  return task;
+}
+}
+  
+  // Leave this code here for the automated tests
 module.exports = {
   PRIORITY, validInteger, validatePriority, todaysDate, ToDo, Task,
+  
 }
+
